9587b39a6768844dd7bd392df47557759c52cd07
kyler-i <kylerittn@gmail.com>
2025-04-27 20:43:23 +0000

    code: Multiples of both three and five

--- src/main/java/KI/FizzBuzz.java
+++ src/main/java/KI/FizzBuzz.java
@@ -15,10 +15,14 @@ import java.util.Random;
 
 public class FizzBuzz {
     public static String convert(int input){
+        if (input % 15 == 0){
+            return "FizzBuzz";
+        }
+        
         if (input % 5 == 0){
             return "Buzz";
         }
-        
+
         if (input % 3 == 0){
             return "Fizz";
         }
